86c8f24865e729bc8b5e2a47a1ae577f66869419
Smiti Shakya <51796883+smitishakya@users.noreply.github.com>
2019-08-15 14:41:55 -0500

    Update index.js

--- catcarousel/index.js
+++ catcarousel/index.js
@@ -1,17 +1,12 @@
 'use strict';
 
 function handleThumbnailClicks() {
-  // eslint-disable-next-line no-undef
   $('.thumbnail').click(event => {
     const imgSrc = $(this).find('img').attr('src');  
      const imgAlt = $(this).find('img').attr('alt');
     
     $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
-    
-    // Accomplishes the same as line 6 with only one use off .attr()
-    // by passing it an object with multiple properties.
-    // See: http://api.jquery.com/attr/#attr-attributes
-    // $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
+ 
   });
 }
 
